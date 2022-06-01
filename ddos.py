17b789e8dc2c966e74f538380873105586d03d0b
xshevc01 <swimmer030203@gmail.com>
2022-06-01 16:18:34 +0200

    utf8

--- ddos.py
+++ ddos.py
@@ -2,15 +2,35 @@ from scapy.all import *
 import codecs
 from suffix_tree import Tree
 
-p = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/MYTEST.pcap")
-print(len(p))
+p1 = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/MYTEST.pcap")
+print(len(p1))
 tree = Tree()
-# b = hexstr(p[0])
-# print(b[len(b) * 3 // 4::])
-for i in range(len(p)):
-    pkt = p[i]
+for i in range(len(p1)):
+    pkt = p1[i]
     a = hexstr(pkt)
     a = a[len(a) * 3 // 4::]
     tree.add(i, a)
 
-print(tree.find('a'))
+# print(tree.find('a'))
+
+# print(p1.summary())
+
+
+pkt = Dot11()
+dns = pkt[Dot11]
+print(dns)
+dns = dns.payload
+print(dns)
+
+print(Ether(p1[0]))
+DNS = p1[0][IP]
+print(DNS)
+
+p2 = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/LEGIT.pcap")
+print(p2[2])
+# legit_tree = Tree()
+# for i in range(len(p2)):
+#     pkt = p2[i]
+#     a = hexstr(pkt)
+#     a = a[len(a) * 3 // 4::]
+#     legit_tree.add(i, a)
--- test_gentree.py
+++ test_gentree.py
@@ -1,10 +1,19 @@
 from suffix_tree import Tree
 
-tree = Tree({1: 'aba', 2: 'bbba'})
+tree = Tree({1: 'aba', 2: 'bbba', 'a' : u'a\x81'})
 print(tree.find('a'), tree.find('cc'))
 tree.add(3, 'ccc')
 print(tree.find('cc'))
 for id_, path in tree.find_all('a'):
     print(id_, ':', str(path))
 # print(tree.to_dot())
-print(tree.find_id(3, 'c'))
+print(tree.find(u'\x81'))
+
+hexstring = '48'
+a_string = bytes.fromhex(hexstring)
+a_string = a_string.decode("ascii")
+
+print(hex(ord("É")))
+tree.add(4, a_string)
+# print(a_string)
+print(u'Entre\x81\xc9')
