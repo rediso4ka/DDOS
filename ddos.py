a18775e79ea55b0fab4b95af6c87270bff20ba49
xshevc01 <swimmer030203@gmail.com>
2022-06-02 15:19:26 +0200

    finally smth

--- ddos.py
+++ ddos.py
@@ -1,36 +1,50 @@
 from scapy.all import *
-import codecs
 from suffix_tree import Tree
 
-p1 = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/MYTEST.pcap")
-print(len(p1))
-tree = Tree()
-for i in range(len(p1)):
-    pkt = p1[i]
-    a = hexstr(pkt)
-    a = a[len(a) * 3 // 4::]
-    tree.add(i, a)
 
-# print(tree.find('a'))
+'''
+Reading legitimate packages
+'''
+legit = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/LEGIT.pcap")
+legit_tree = Tree()
+for i in range(100):
+    pkt = legit[i]
+    try:
+        payload = pkt[Raw]
+        legit_tree.add(str(i), bytes(payload))
+    except LookupError:
+        continue
 
-# print(p1.summary())
+
+'''
+Reading DNS packages
+'''
+dns = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/DNS.pcap")
+dns_tree = Tree()
+for i in range(100):
+    pkt = dns[i]
+    try:
+        payload = pkt[Raw]
+        dns_tree.add(str(i), bytes(payload))
+    except LookupError:
+        continue
 
 
-pkt = Dot11()
-dns = pkt[Dot11]
-print(dns)
-dns = dns.payload
-print(dns)
-
-print(Ether(p1[0]))
-DNS = p1[0][IP]
-print(DNS)
-
-p2 = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/LEGIT.pcap")
-print(p2[2])
-# legit_tree = Tree()
-# for i in range(len(p2)):
-#     pkt = p2[i]
-#     a = hexstr(pkt)
-#     a = a[len(a) * 3 // 4::]
-#     legit_tree.add(i, a)
+'''
+Testing
+'''
+# mytest = rdpcap("/home/aleksandr/new_storage/DDOS/datasets/MYTEST.pcap")
+# mytest_tree = Tree()
+# for i in range(len(mytest)):
+#     pkt = mytest[i]
+#     try:
+#         payload = pkt[Raw]
+#         print(i, ": ", bytes(payload), type(payload))
+#         mytest_tree.add(str(i), bytes(payload))
+#     except:
+#         continue
+#
+# print("\n")
+# print(mytest_tree.find(b'\x00'))
+# for id_, path in mytest_tree.find_all(b'\xcd'):
+#     print(id_, ':', str(path))
